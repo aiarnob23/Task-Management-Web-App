b9ca0c3afcebef1c5f234308cd591c1548540657
aiarnob23 <aiarnob23@gmail.com>
2025-07-31 10:53:28 +0600

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
-import cors from 'cors';
-import express from 'express';
-import { userRoutes } from './routes/user.routes.js';
-import { taskRoutes } from './routes/task.routes.js';
+import cors from "cors";
+import express from "express";
+import { userRoutes } from "./routes/user.routes.js";
+import { taskRoutes } from "./routes/task.routes.js";
 import helmet from "helmet";
 
 //cors and middlewares
@@ -9,25 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
-app.use(cors({
-    origin: ["http://localhost:5173"],
+app.use(
+  cors({
+    origin: [
+      "https://task-management-web-app-pink.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-    allowedHeaders:['Content-Type' , 'Authorization', 'X-Requested-With'],
-  }));
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
 
 // routes
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/task', taskRoutes);
-app.get('/',(req ,res )=>{
-    res.send('Task Management Web Application Server');
-})
-app.use((req,res,next)=>{
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/task", taskRoutes);
+app.get("/", (req, res) => {
+  res.send("Task Management Web Application Server");
+});
+app.use((req, res, next) => {
   res.status(404).json({
-    success:false,
-    message:`Resource not found`,
-    error:{code:'NOT_FOUND', details: 'The requested URL does not exist on this server.'}
-  })
-})
+    success: false,
+    message: `Resource not found`,
+    error: {
+      code: "NOT_FOUND",
+      details: "The requested URL does not exist on this server.",
+    },
+  });
+});
 
 export default app;
